694ec5f20be2e7f250e106609f8e377b3f3aef9f
Russell Valentine <russ@coldstonelabs.org>
2015-12-13 13:29:08 -0600

    start of process script

--- test_jig_lsm303c/process.py
+++ test_jig_lsm303c/process.py
@@ -9,24 +9,24 @@ rows = []
 with open(sys.argv[1], 'r') as f:
     sr = csv.reader(f, delimiter=",", quotechar='"')
     for row in sr:
-        d = [float(sr[0]), float(row[1]), float(row[2]), float(row[3]), 0.0, 0.0, 0.0, 0.0]
-        rows.append(row)
+        d = [float(row[0]), float(row[1])/1000.0, float(row[2])/1000.0, float(row[3])/1000.0, 0.0, 0.0, 0.0, 0.0]
+        rows.append(d)
 
 rows = numpy.asarray(rows)
 
 pt = rows[0][0]
 for row in rows:
-    pt2 = row[0]
+    #pt2 = row[0]
     row[0] = row[0] - pt
-    pt = pt2
+    #pt = pt2
     pitch=math.atan(row[1]/math.sqrt(row[2]**2+row[3]**2))
     roll=math.atan(row[2]/math.sqrt(row[1]**2+row[3]**2))
     if row[3] == 0.0:
         theta = math.pi/2.0
     else:
         theta=math.atan(math.sqrt(row[1]**2+row[2]**2)/row[3])
-    row[4] = pitch
-    row[5] = roll
-    row[6] = theta
+    row[4] = pitch * 180.0/math.pi
+    row[5] = roll * 180.0/math.pi
+    row[6] = theta * 180.0/math.pi
     row[7] = math.sqrt(row[1]**2+row[2]**2+row[3]**2)
-
+    print row
